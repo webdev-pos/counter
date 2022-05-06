d7b9440138904c44a66dc33f82e48b38980de780
Nick <105077694+NichlasBach@users.noreply.github.com>
2022-05-06 23:13:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const inputEl = document.getElementById("input");
+const addButton = document.getElementById("add");
+const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
+const countEl = document.getElementById("count");
+const resetEl = document.getElementById("reset");
+
+let count = 0;
+
+
+increaseButton.addEventListener("click", function() {
+    count++;
+    countEl.textContent = "Count: " + count;
+})
+
+decreaseButton.addEventListener("click", function() {
+    count--;
+    countEl.textContent = "Count: " + count;
+})
+
+resetEl.addEventListener("click", function() {
+    count = 0;
+    countEl.textContent = "Count: " + count;
+})
+
+addButton.addEventListener("click", function() {
+    let calc = count + parseInt(inputEl.value);
+    count = calc
+    countEl.textContent = "Count: " + calc;
+    inputEl.value = "";
+})
